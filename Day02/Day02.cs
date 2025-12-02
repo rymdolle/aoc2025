92c038ea0b4d345cacb1e6f27fa33e62eac71fb9
Olle Mattsson <rymdolle@users.noreply.github.com>
2025-12-02 07:14:57 +0100

    Day 2

---
+++ Day02/Day02.cs
@@ -0,0 +1,51 @@
+long part1 = 0;
+long part2 = 0;
+File.ReadAllLines(args.Length > 0 ? args[0] : "input.txt").ToList().ForEach(line => {
+    line.Split(',').ToList().ForEach(range => {
+        var parts = range.Split('-');
+        for (long i = long.Parse(parts[0]); i <= long.Parse(parts[1]); i++)
+        {
+            string id = i.ToString();
+
+            // part 1
+            if (id.Length % 2 == 0)
+            {
+                int j = id.Length / 2;
+                string a = id[..j];
+                string b = id[j..];
+                if (a == b)
+                    part1 += i;
+            }
+
+            // part 2
+            for (int j = 1; j < id.Length; j++)
+            {
+                string a = id[..j];
+                string b = id[j..];
+                if (FindRepeat(a, b))
+                {
+                    part2 += i;
+                    break;
+                }
+            }
+        }
+        return;
+    });
+});
+
+bool FindRepeat(string a, string b)
+{
+    if (a.Length == 0)
+        return b.Length == 0;
+    if (b.Length % a.Length != 0)
+        return false;
+    for (int i = 0; i < b.Length; i += a.Length)
+    {
+        if (a != b.Substring(i, a.Length))
+            return false;
+    }
+    return true;
+}
+
+Console.WriteLine(part1);
+Console.WriteLine(part2);
