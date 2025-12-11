06334919cfdb43b1355df12ccdfa6160ced5a5f3
Olle Mattsson <rymdolle@users.noreply.github.com>
2025-12-11 18:20:49 +0100

    Day 11 part 2

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -5,10 +5,10 @@ var cords = File.ReadAllLines(args.Length > 0 ? args[0] : "input.txt")
     .Select(s => s.Split(":"))
     .ToDictionary(s => s[0], s => s[1].Split(" ", StringSplitOptions.RemoveEmptyEntries));
 
-Queue<(string, List<string>)> q = new(cords["you"].Select(s => (s, new List<string>())));
+Queue<string> q = new(cords["you"]);
 while (q.Count > 0)
 {
-    var (key, path) = q.Dequeue();
+    var key = q.Dequeue();
     if (key == "out")
     {
         part1++;
@@ -16,8 +16,21 @@ while (q.Count > 0)
     }
 
     foreach (var next in cords[key])
-        q.Enqueue((next, [..path, next]));
+        q.Enqueue(next);
 }
+
+Dictionary<(string, bool, bool), long> memo = new();
+long FindNodeViaDacFft(string start, bool dac = false, bool fft = false)
+{
+    if (start == "out")
+        return dac && fft ? 1 : 0;
+    if (memo.TryGetValue((start, dac, fft), out var value))
+        return value;
+    return memo[(start, dac, fft)] = cords[start]
+        .Sum(s => FindNodeViaDacFft(s, dac || s == "dac", fft || s == "fft"));
+}
+
+part2 += FindNodeViaDacFft("svr");
 
 Console.WriteLine(part1);
 Console.WriteLine(part2);
